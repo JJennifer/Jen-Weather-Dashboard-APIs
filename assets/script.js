0b432e88daaa9f6c5b8c7f1f7d26e990368b23c8
JJ <jenniferj1796@gmail.com>
2023-04-15 13:57:28 -0400

    added JS code

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,194 @@
 var apikey = "05e883ab5e1a5aa9dbb8f6d3c975191f";
 var url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`
-var city
+var textInput = document.querySelector(".text");
+var city = textInput.value;
+var days = document.getElementById("today")
+var futureDays = document.getElementById("future")
+var futureDays1 = document.getElementById("future1")
+var futureDays2 = document.getElementById("future2")
+var futureDays3 = document.getElementById("future3")
+var futureDays4 = document.getElementById("future4")
 
 
 
 var searchButton = document.querySelector(".searchContainer");
-var textInput = document.querySelector(".text ");
+
 console.log(textInput);
 
 searchButton.addEventListener("submit", function(event) {
     event.preventDefault();
+    var city = textInput.value;
+
     console.log("this button works");
-    console.log(textInput.value);
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${"city"}&appid=05e883ab5e1a5aa9dbb8f6d3c975191f`)
+    // console.log(textInput.value);
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apikey}`)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data);
-        console.log(data.city.name);
-        console.log(data.list.dt_txt);
-        console.log(data.list[5].main.temp);
-        console.log(data.list[5].main.humidity);
-        console.log(data.list[5].wind.speed);
-        console.log(data.list[5].weather[3]);
-    })
+        // console.log(data.city.name);
+        // console.log(data.list[5].main.temp);
+        // console.log(data.list[5].main.humidity);
+        // console.log(data.list[5].wind.speed);
+        
+        var todayForecast = document.createElement("div");
+        todayForecast.textContent = data.city.name;
+        days.appendChild(todayForecast);
 
+        var date = document.createElement("p");
+        date.textContent = "Date & Time: " + data.list[0].dt_txt;
+        todayForecast.appendChild(date);
+
+        var icon = document.createElement("p");
+        icon.textContent = "Icon: " + data.list[0].weather[0].icon;
+        todayForecast.appendChild(icon);
+
+        var temp = document.createElement("p");
+        temp.textContent = "Temperture: " + data.list[0].main.temp + "F";
+        todayForecast.appendChild(temp);
+
+        var humidity = document.createElement("p");
+        humidity.textContent = "Humidity: " + data.list[0].main.humidity;
+        todayForecast.appendChild(humidity);
+
+        var windSpeed = document.createElement("p");
+        windSpeed.textContent = "Wind-Speed: " + data.list[0].wind.speed;
+        todayForecast.appendChild(windSpeed);
+        
+    })
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apikey}`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+       
+        // Next day forecast 
+        var todayForecast = document.createElement("div");
+        todayForecast.textContent = data.city.name;
+        futureDays.appendChild(todayForecast);
+
+        var date = document.createElement("p");
+        date.textContent = "Date & Time: " + data.list[3].dt_txt;
+        todayForecast.appendChild(date);
+
+        var icon = document.createElement("p");
+        icon.textContent = "Icon: " + data.list[3].weather[0].icon;
+        todayForecast.appendChild(icon);
+
+        var temp = document.createElement("p");
+        temp.textContent = "Temperture: " + data.list[3].main.temp + "F";
+        todayForecast.appendChild(temp);
+
+        var humidity = document.createElement("p");
+        humidity.textContent = "Humidity: " + data.list[3].main.humidity;
+        todayForecast.appendChild(humidity);
+
+        var windSpeed = document.createElement("p");
+        windSpeed.textContent = "Wind-Speed: " + data.list[3].wind.speed;
+        todayForecast.appendChild(windSpeed);
+        // day 2 forecast
+        var todayForecast = document.createElement("div");
+        todayForecast.textContent = data.city.name;
+        futureDays1.appendChild(todayForecast);
+
+        var date = document.createElement("p");
+        date.textContent = "Date & Time: " + data.list[6].dt_txt;
+        todayForecast.appendChild(date);
+
+        var icon = document.createElement("p");
+        icon.textContent = "Icon: " + data.list[6].weather[0].icon;
+        todayForecast.appendChild(icon);
+
+        var temp = document.createElement("p");
+        temp.textContent = "Temperture: " + data.list[6].main.temp + "F";
+        todayForecast.appendChild(temp);
+
+        var humidity = document.createElement("p");
+        humidity.textContent = "Humidity: " + data.list[6].main.humidity;
+        todayForecast.appendChild(humidity);
+
+        var windSpeed = document.createElement("p");
+        windSpeed.textContent = "Wind-Speed: " + data.list[6].wind.speed;
+        todayForecast.appendChild(windSpeed);
+        // Day 3 forcast
+        var todayForecast = document.createElement("div");
+        todayForecast.textContent = data.city.name;
+        futureDays2.appendChild(todayForecast);
+
+        var date = document.createElement("p");
+        date.textContent = "Date & Time: " + data.list[6].dt_txt;
+        todayForecast.appendChild(date);
+
+        var icon = document.createElement("p");
+        icon.textContent = "Icon: " + data.list[6].weather[0].icon;
+        todayForecast.appendChild(icon);
+
+        var temp = document.createElement("p");
+        temp.textContent = "Temperture: " + data.list[6].main.temp + "F";
+        todayForecast.appendChild(temp);
+
+        var humidity = document.createElement("p");
+        humidity.textContent = "Humidity: " + data.list[6].main.humidity;
+        todayForecast.appendChild(humidity);
+
+        var windSpeed = document.createElement("p");
+        windSpeed.textContent = "Wind-Speed: " + data.list[6].wind.speed;
+        todayForecast.appendChild(windSpeed);
+         // Day 4 forcast
+         var todayForecast = document.createElement("div");
+         todayForecast.textContent = data.city.name;
+         futureDays3.appendChild(todayForecast);
+ 
+         var date = document.createElement("p");
+         date.textContent = "Date & Time: " + data.list[6].dt_txt;
+         todayForecast.appendChild(date);
+ 
+         var icon = document.createElement("p");
+         icon.textContent = "Icon: " + data.list[6].weather[0].icon;
+         todayForecast.appendChild(icon);
+ 
+         var temp = document.createElement("p");
+         temp.textContent = "Temperture: " + data.list[6].main.temp + "F";
+         todayForecast.appendChild(temp);
+ 
+         var humidity = document.createElement("p");
+         humidity.textContent = "Humidity: " + data.list[6].main.humidity;
+         todayForecast.appendChild(humidity);
+ 
+         var windSpeed = document.createElement("p");
+         windSpeed.textContent = "Wind-Speed: " + data.list[6].wind.speed;
+         todayForecast.appendChild(windSpeed);
+          // Day 5 forcast
+        var todayForecast = document.createElement("div");
+        todayForecast.textContent = data.city.name;
+        futureDays4.appendChild(todayForecast);
+
+        var date = document.createElement("p");
+        date.textContent = "Date & Time: " + data.list[6].dt_txt;
+        todayForecast.appendChild(date);
+
+        var icon = document.createElement("p");
+        icon.textContent = "Icon: " + data.list[6].weather[0].icon;
+        todayForecast.appendChild(icon);
+
+        var temp = document.createElement("p");
+        temp.textContent = "Temperture: " + data.list[6].main.temp + "F";
+        todayForecast.appendChild(temp);
+
+        var humidity = document.createElement("p");
+        humidity.textContent = "Humidity: " + data.list[6].main.humidity;
+        todayForecast.appendChild(humidity);
+
+        var windSpeed = document.createElement("p");
+        windSpeed.textContent = "Wind-Speed: " + data.list[6].wind.speed;
+        todayForecast.appendChild(windSpeed);
+        
+    })
 })
+
+
 
 
 
